3dd117b3ecee5e794354f1a8364d8d7ee2928a5a
Greatman Lim <greatman.lim@mahti.org>
2018-11-30 06:23:31 +0200

    Always push master

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 source ~/Documents/passwords/community.mahti.org
+git push origin master
 hugo
 lftp -c "open -u mahti,$PASSWORD ftp.mahti.org; set ftp:ssl-allow no; set ssl:verify-certificate no; mirror -R public/ ~/public_html/subdomains/community/"
